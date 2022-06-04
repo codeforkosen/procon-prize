579308d252a002af31beb42263b97f803e16ae81
Taisuke Fukuno <fukuno@jig.jp>
2022-06-05 07:58:39 +0900

    simplify

--- didbbs.js
+++ didbbs.js
@@ -1,32 +1,8 @@
 import { Server } from "https://js.sabae.cc/Server.js";
 import { JSONDB } from "https://js.sabae.cc/JSONDB.js";
-import { Ed25519 } from "https://code4fukui.github.io/Ed25519/Ed25519.js";
-import { Text } from "https://code4fukui.github.io/Ed25519/Text.js";
-import { DIDKey } from "https://code4fukui.github.io/Ed25519/DIDKey.js";
+import { checkSign } from "https://code4fukui.github.io/Ed25519/checkSign.js";
 
-const port = Deno.args[0] || 8001;
 const bbs = new JSONDB("bbs.json");
-
-const checkSign = (did, body) => { // throws
-  const publicKey = DIDKey.decode(did)?.data;
-  if (!publicKey) {
-    throw "no publicKey";
-  }
-  const sign = Text.extractSign(body);
-  if (!sign) {
-    throw "no sign";
-  }
-  const signature = DIDKey.decode(sign)?.data;
-  if (!signature) {
-    throw "sign not match";
-  }
-  console.log(signature);
-  const message = Text.encode(Text.normalize(body));
-  const chk = Ed25519.verify({ signature, publicKey, message, encoding: "binary" });
-  if (!chk) {
-    throw "illegal sign";
-  }
-};
 
 class MyServer extends Server {
   api(path, req) {
@@ -34,19 +10,16 @@ class MyServer extends Server {
       return bbs.data;
     } else if (path == "/api/add") {
       try {
-        console.log(req);
-        const did = req.did;
-        const body = req.body;
-        checkSign(did, body);
+        checkSign(req.did, req.body);
         bbs.data.push(req);
         bbs.write();
         return "ok";
       } catch (e) {
-        console.log(e);
         return e;
       }
     }
   }
 }
 
+const port = Deno.args[0] || 8001;
 new MyServer(port);
