a356126881ed89ae1b6d69cff63859d263f59668
Taisuke Fukuno <fukuno@jig.jp>
2022-10-16 04:40:28 +0900

    add signature & verify by Ed25519

--- proconbbs.js
+++ proconbbs.js
@@ -1,7 +1,8 @@
 import { Server } from "https://js.sabae.cc/Server.js";
 import { JSONDB } from "https://js.sabae.cc/JSONDB.js";
-import { checkSign } from "https://code4fukui.github.io/Ed25519/checkSign.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
+import { Ed25519 } from "https://code4fukui.github.io/Ed25519/Ed25519.js";
+import { Base16 } from "https://code4fukui.github.io/Base16/Base16.js";
 
 const bbs = new JSONDB("bbs.json");
 
@@ -18,16 +19,26 @@ if (bbs.data.length == 0) {
   bbs.write();
 }
 
+const verify = (did, text, sign) => {
+  const message = new TextEncoder().encode(text);
+  //const signature = Ed25519.sign({ privateKey: user.secret, message, encodeing: "binary" });
+  const chk = Ed25519.verify({ signature: Base16.decode(sign), publicKey: Base16.decode(did), message, encoding: "binary" });
+  //console.log(chk);
+  return chk;
+};
+
 class MyServer extends Server {
   api(path, req) {
     if (path == "/api/list") {
       return bbs.data;
     } else if (path == "/api/add") {
       try {
-        console.log(req);
-        //checkSign(req.did, req.body);
+        if (req.sign) {
+          if (!verify(req.did, req.body + "\n" + req.date, req.sign)) {
+            throw new Error("illegal sign");
+          }
+        }
         const topic = bbs.data.find(d => d.id == req.id);
-        console.log(topic);
         topic.items.push(req);
         bbs.write();
         return "ok";
