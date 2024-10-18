8d13bab3a07030a7b228b3bad1d22da775b2acb5
Taisuke Fukuno <fukuno@jig.jp>
2024-10-19 06:24:41 +0900

    update for Deno2

--- proconbbs.js
+++ proconbbs.js
@@ -1,4 +1,4 @@
-import { Server } from "https://js.sabae.cc/Server.js";
+import { fetchWeb } from "https://code4fukui.github.io/wsutil/fetchWeb.js";
 import { JSONDB } from "https://js.sabae.cc/JSONDB.js";
 import { Ed25519 } from "https://code4fukui.github.io/Ed25519/Ed25519.js";
 import { Base16 } from "https://code4fukui.github.io/Base16/Base16.js";
@@ -13,34 +13,29 @@ const verify = (did, text, sign) => {
   return chk;
 };
 
-class MyServer extends Server {
-  api(path, req) {
-    if (path == "/api/list") {
-      return bbs.data;
-    } else if (path == "/api/add") {
-      try {
-        if (req.sign) {
-          if (!verify(req.did, req.body + "\n" + req.date, req.sign)) {
-            throw new Error("illegal sign");
-          }
+export default fetchWeb(async (param, req, path, conninfo) => {
+  if (path == "/api/list") {
+    return bbs.data;
+  } else if (path == "/api/add") {
+    try {
+      if (param.sign) {
+        if (!verify(param.did, param.body + "\n" + param.date, param.sign)) {
+          throw new Error("illegal sign");
         }
-        const type = req.type;
-        const num = req.num;
-        const topic = bbs.data.find(d => d.type == type && d.num == num);
-        if (!topic) {
-          bbs.data.push({ type, num, items: [req] });
-        } else {
-          topic.items.push(req);
-        }
-        bbs.write();
-        return "ok";
-      } catch (e) {
-        console.log(e);
-        return e;
       }
+      const type = param.type;
+      const num = param.num;
+      const topic = bbs.data.find(d => d.type == type && d.num == num);
+      if (!topic) {
+        bbs.data.push({ type, num, items: [req] });
+      } else {
+        topic.items.push(req);
+      }
+      bbs.write();
+      return "ok";
+    } catch (e) {
+      console.log(e);
+      return e;
     }
   }
-}
-
-const port = Deno.args[0] || 8001;
-new MyServer(port);
+});
